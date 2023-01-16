478e2b1bae63ff4fa664748ec5807e7459f7445a
Sejeong Hong <dev.gwisket@gmail.com>
2023-01-17 01:17:59 +0900

    init firebase

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-console.log('hello, world!')
+const firebaseConfig = {
+  apiKey: "AIzaSyDn3gAjJyDAgkgMVJgSB-pPwazWrE1i9xg",
+  authDomain: "fir-practice-a3876.firebaseapp.com",
+  projectId: "fir-practice-a3876",
+  storageBucket: "fir-practice-a3876.appspot.com",
+  messagingSenderId: "303182350871",
+  appId: "1:303182350871:web:a05905cf51cfb0a30bea52"
+};
 
